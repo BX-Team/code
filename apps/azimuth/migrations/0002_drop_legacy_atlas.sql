4c15d43abf6ccbdb05aaf7cc7428e91cf99839cb
NONPLAYT <nonplay@bxteam.org>
2026-09-06 03:31:20 +0300

    chore(azimuth): add drop migration

---
+++ apps/azimuth/migrations/0002_drop_legacy_atlas.sql
@@ -0,0 +1,5 @@
+drop table if exists atlas_downloads;
+drop table if exists atlas_commits;
+drop table if exists atlas_builds;
+drop table if exists atlas_versions;
+drop table if exists atlas_projects;
